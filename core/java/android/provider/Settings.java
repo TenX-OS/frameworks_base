a6b189923c80cb0376c2d29c4251d57478c049a5
Altaf-Mahdi <altaf.mahdi@gmail.com>
2015-10-23 23:44:43 -0400

    base: Hide power menu on secure lockscreen [1/2]
    Adds an option to enable/disable power menu on secure lockscreen.
    
    Change-Id: I6f0e1e5bf436debda5b241ee1040dd433d9a9569
    Signed-off-by: Aston-Martinn <advaithbhat9@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -10028,6 +10028,13 @@ public final class Settings {
                 "lock_screen_transparent_notifications_enabled";
 
         /**
+         * Disable power menu on secure lock screens
+         *
+         * @hide
+         */
+        public static final String POWER_MENU_HIDE_ON_SECURE = "power_menu_hide_on_secure";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
--- services/core/java/com/android/server/policy/PhoneWindowManager.java
+++ services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -640,6 +640,9 @@ public class PhoneWindowManager implements WindowManagerPolicy {
     // Maps global key codes to the components that will handle them.
     private GlobalKeyManager mGlobalKeyManager;
 
+    // Hide power menu on secure lockscreen
+    private boolean mHideGlobalActionsOnSecure;
+
     // Fallback actions by key code.
     private final SparseArray<KeyCharacterMap.FallbackAction> mFallbackActions =
             new SparseArray<KeyCharacterMap.FallbackAction>();
@@ -928,6 +931,9 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                         UserHandle.USER_ALL);
             }
 
+            resolver.registerContentObserver(Settings.Secure.getUriFor(
+                    Settings.Secure.POWER_MENU_HIDE_ON_SECURE), false, this,
+                    UserHandle.USER_ALL);
             updateSettings();
         }
 
@@ -1674,10 +1680,14 @@ public class PhoneWindowManager implements WindowManagerPolicy {
     }
 
     void showGlobalActionsInternal() {
+        final boolean keyguardShowing = isKeyguardShowingAndNotOccluded();
+        if (keyguardShowing && isKeyguardSecure(mCurrentUserId) &&
+                mHideGlobalActionsOnSecure) {
+            return;
+        }
         if (mGlobalActions == null) {
             mGlobalActions = new GlobalActions(mContext, mWindowManagerFuncs);
         }
-        final boolean keyguardShowing = isKeyguardShowingAndNotOccluded();
         mGlobalActions.showDialog(keyguardShowing, isDeviceProvisioned());
         // since it took two seconds of long press to bring this up,
         // poke the wake lock so they have some time to see the dialog.
@@ -2480,9 +2490,9 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                     Settings.Global.POWER_BUTTON_VERY_LONG_PRESS,
                     mContext.getResources().getInteger(
                             com.android.internal.R.integer.config_veryLongPressOnPowerBehavior));
-            mTorchActionMode = Settings.Secure.getIntForUser(resolver,
-                    Settings.Secure.TORCH_POWER_BUTTON_GESTURE, 0,
-                    UserHandle.USER_CURRENT);
+            mHideGlobalActionsOnSecure = Settings.Secure.getIntForUser(resolver,
+                    Settings.Secure.POWER_MENU_HIDE_ON_SECURE,
+                            0, UserHandle.USER_CURRENT) != 0;
         }
         if (updateRotation) {
             updateRotation(true);
