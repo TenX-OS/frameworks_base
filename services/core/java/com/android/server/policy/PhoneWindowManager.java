070c62d5d6d4789b622e89474f8f92e1df24bb50
Simon Shields <simon@lineageos.org>
2017-11-24 15:59:49 +1100

    base: Add LineageButtons (volume skip track hooks)
    --
    abcu: move to main framework
    --
    
    This class will be the new home for all the buttons logic currently
    in fw/b. Currently, it only implements the "long press volume keys to
    skip" feature.
    
    Based on the following commits from cm-14.1:
    
    commit 1296fb316946ca1361b2d32557b6c9e028f509c8
    Author: Dark Eyes <dark_eyes_@hotmail.com>
    Date:   Sat Nov 5 12:09:31 2016 -0400
    
        Fix long press volume buttons skip tracks
    
        Without that patch, the volume changes up or down prior
        to skipping tracks because the sendvolumekeyevent doesn't
        have the mayChangeVolume condition. Also, that part is
        useless cause the mUseTvRouting condition is taken care
        of correctly later.
    
        Change-Id: I88757315f135f013e7692861d46b3a07fbfc07d5
    
    commit 61cebe2fea95f799b7b4edc540f6c38d3c13fbde
    Author: Michael Bestas <mikeioannina@gmail.com>
    Date:   Thu Dec 18 14:43:22 2014 +0200
    
        Fix volume key music controls and wake up
    
        - Forward port code from cm-11.0 and adjust for 5.0
        - Fix not being able to adjust volume when music control is on
        - Disable screen off volume/music control when wake key is enabled
    
        Change-Id: Ie0ad83965c41fd33120490c25a4ff3d9b2cbeb37
    
    commit f41ba8af1796b4903dba3d134e56e8148f3b4808
    Author: Austin T. Conn <atc3030dev@gmail.com>
    Date:   Wed Dec 17 13:28:00 2014 -0500
    
        Settings: Forward Port Volume Long Press Skip Tracks (1/2)
    
        Bring Forward Volume Rocker Track Change
    
        Requires Settings Change
        http://review.cyanogenmod.org/#/c/82222/
    
        Change-Id: I70b00e08fe48707110725dd1daa56b1e48d15a03
    
    Change-Id: I5c7f733913242bba822cf3f9f7e1e3320c2bedf7
    
    PhoneWindowManager: add LineageButtons volumekey hook
    
    We can't use a generic DeviceKeyHandler here, because we don't want to
    override some of the AOSP logic (silencing incoming calls, screenshots)
    and we need to know if the device is interactive or not (for screen-off
    music controls)
    
    Change-Id: I485c2f6006c5bbd358ba0cbd32917689826c5c6d
    Signed-off-by: Aston-Martinn <advaithbhat9@gmail.com>

--- services/core/java/com/android/server/policy/PhoneWindowManager.java
+++ services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -206,6 +206,7 @@ import com.android.internal.policy.KeyInterceptionInfo;
 import com.android.internal.policy.PhoneWindow;
 import com.android.internal.statusbar.IStatusBarService;
 import com.android.internal.util.ArrayUtils;
+import com.android.internal.util.custom.LineageButtons;
 import com.android.server.ExtconStateObserver;
 import com.android.server.ExtconUEventObserver;
 import com.android.internal.util.custom.Utils;
@@ -655,6 +656,8 @@ public class PhoneWindowManager implements WindowManagerPolicy {
     private int mPowerButtonSuppressionDelayMillis = POWER_BUTTON_SUPPRESSION_DELAY_DEFAULT_MILLIS;
 
     private int mTorchActionMode;
+
+    private LineageButtons mLineageButtons;
 
     private final List<DeviceKeyHandler> mDeviceKeyHandlers = new ArrayList<>();
 
@@ -4380,6 +4383,10 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                     // {@link interceptKeyBeforeDispatching()}.
                     result |= ACTION_PASS_TO_USER;
                 } else if ((result & ACTION_PASS_TO_USER) == 0) {
+                    if (mLineageButtons.handleVolumeKey(event, interactive)) {
+                        break;
+                    }
+
                     // If we aren't passing to the user and no one else
                     // handled it send it to the session manager to
                     // figure out.
@@ -5595,6 +5602,8 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                 mKeyguardDelegate.onBootCompleted();
             }
         }
+
+        mLineageButtons = new LineageButtons(mContext);
 
         mAutofillManagerInternal = LocalServices.getService(AutofillManagerInternal.class);
     }
