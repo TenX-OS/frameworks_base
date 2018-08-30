966963bf7a90cd7f6b38350387a00a22cfd64ff7
maxwen <max.weninger@gmail.com>
2018-08-30 22:21:34 +0200

    base: Keyguard weather view [1/3]
    Change-Id: Idbedea20d5487a42849c225e3f500aa1f0282c6c
    Signed-off-by: Aston-Martinn <advaithbhat9@gmail.com>

--- packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
+++ packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
@@ -247,7 +247,7 @@ public class QSFactoryImpl implements QSFactory {
             case "onthego":
                 return mOnTheGoTileProvider.get();
             case "weather":
-                return mWeatherTile.get();
+                return mWeatherTileProvider.get();
         }
 
         // Custom tiles
