f461f13f0e907e3dd7f31df14245aa2c7b080ed4
Lucchetto <lucchetto.tie@live.com>
2019-03-28 18:41:03 +0100

    base: Smart Charging (1/4)
    Signed-off-by: Aston-Martinn <advaithbhat9@gmail.com>
    Change-Id: I709014a7ebcfb35694f5a91ba2c87b67159f25ff

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4915,6 +4915,16 @@ public final class Settings {
                 "high_touch_sensitivity_enable";
 
         /**
+         * @hide
+         */
+        public static final String SMART_CHARGING = "smart_charging";
+
+        /**
+         * @hide
+         */
+        public static final String SMART_CHARGING_LEVEL = "smart_charging_level";
+
+        /**
          * IMPORTANT: If you add a new public settings you also have to add it to
          * PUBLIC_SETTINGS below. If the new setting is hidden you have to add
          * it to PRIVATE_SETTINGS below. Also add a validator that can validate
