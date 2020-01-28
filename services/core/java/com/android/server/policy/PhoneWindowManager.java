d8c3aa30304c562139ef77c7ac7f2a084ef7c0d3
Sam Mortimer <sam@mortimer.me.uk>
2020-01-28 00:47:27 -0800

    base: Let long press power turn torch off when screen is on.
    * When the torch is on, any subsequent long press power is almost certainly
      intended to turn the torch off (regardless of screen state).  Therefore,
      always allow long press power to toggle torch if the torch is on.
    
    * Tested: long press power toggles torch on/off with screen off.
              long press power toggles torch off with screen on and torch on.
              long press power brings up global actions menu with screen on and torch off.
    
    Change-Id: I932caa9f3be06d14408aea2ecb3a6eca73e052e0
    Signed-off-by: Aston-Martinn <advaithbhat9@gmail.com>

--- services/core/java/com/android/server/policy/PhoneWindowManager.java
+++ services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -1556,7 +1556,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
         if (mPocketLockShowing) {
             return LONG_PRESS_POWER_HIDE_POCKET_LOCK;
         }
-        if (mTorchLongPressPowerEnabled && !isScreenOn()) {
+        if (mTorchLongPressPowerEnabled && (!isScreenOn() || isDozeMode() || mTorchEnabled)) {
             return LONG_PRESS_POWER_TORCH;
         }
         return mLongPressOnPowerBehavior;
