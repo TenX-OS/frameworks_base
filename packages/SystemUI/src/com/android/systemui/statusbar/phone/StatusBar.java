2bb872a4d2110581f37c058c7dab9d33519bed91
Joey Huab <joey@evolution-x.org>
2020-02-20 13:05:55 +0000

    SystemUI: Add a toggle for the new QS tint [1/2]
    **SDB - Adapt for Gradient Views
    
    Signed-off-by: Aston-Martinn <advaithbhat9@gmail.com>
    Change-Id: Ibe868d2e1dc21d2f0deabaa51710f723a663ee49

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -2097,6 +2097,9 @@ public class StatusBar extends SystemUI implements DemoMode,
             resolver.registerContentObserver(Settings.Secure.getUriFor(
                     Settings.Secure.CENTER_TEXT_CLOCK),
                     false, this, UserHandle.USER_ALL);
+            resolver.registerContentObserver(Settings.System.getUriFor(
+                    Settings.System.QS_PANEL_BG_USE_NEW_TINT),
+                    false, this, UserHandle.USER_ALL);
         }
 
         @Override
@@ -2126,6 +2129,9 @@ public class StatusBar extends SystemUI implements DemoMode,
                       uri.equals(Settings.Secure.getUriFor(
                     Settings.Secure.CENTER_TEXT_CLOCK))) {
                 updateKeyguardStatusSettings();
+            } else if (uri.equals(Settings.System.getUriFor(
+                    Settings.System.QS_PANEL_BG_USE_NEW_TINT))) {
+                mQSPanel.getHost().reloadAllTiles();
             }
         }
 
