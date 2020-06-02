5120640259eaebd26d149b02e340f0ab43097616
SpiritCroc <dev@spiritcroc.de>
2020-06-02 12:41:00 +0200

    base: [tenxUtils] Import SystemUI Restart API
    Signed-off-by: spezi77 <spezi7713@gmx.net>
    Signed-off-by: Aston-Martinn <advaithbhat9@gmail.com>
    Change-Id: I03715be8271327db2d960506517c3298cb4db638

--- core/java/com/android/internal/util/tenx/tenxUtils.java
+++ core/java/com/android/internal/util/tenx/tenxUtils.java
@@ -18,8 +18,11 @@ package com.android.internal.util.tenx;
 
 import android.app.NotificationManager;
 import android.app.ActivityManager;
+import android.app.AlertDialog;
+import android.app.IActivityManager;
 import android.Manifest;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.om.IOverlayManager;
 import android.content.om.OverlayInfo;
@@ -39,6 +42,7 @@ import android.os.SystemProperties;
 import android.text.format.Time;
 import android.os.SystemClock;
 import android.net.NetworkInfo;
+import android.os.AsyncTask;
 import android.os.UserHandle;
 import android.os.Vibrator;
 import android.provider.MediaStore;
@@ -357,6 +361,50 @@ public class tenxUtils {
 
     public static boolean isPackageInstalled(Context context, String pkg) {
         return isPackageInstalled(context, pkg, true);
+    }
+
+    public static void restartSystemUi(Context context) {
+        new RestartSystemUiTask(context).execute();
+    }
+
+    public static void showSystemUiRestartDialog(Context context) {
+        new AlertDialog.Builder(context)
+                .setTitle(R.string.systemui_restart_title)
+                .setMessage(R.string.systemui_restart_message)
+                .setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        restartSystemUi(context);
+                    }
+                })
+                .setNegativeButton(R.string.cancel, null)
+                .show();
+    }
+
+    private static class RestartSystemUiTask extends AsyncTask<Void, Void, Void> {
+        private Context mContext;
+
+        public RestartSystemUiTask(Context context) {
+            super();
+            mContext = context;
+        }
+
+        @Override
+        protected Void doInBackground(Void... params) {
+            try {
+                ActivityManager am =
+                        (ActivityManager) mContext.getSystemService(Context.ACTIVITY_SERVICE);
+                IActivityManager ams = ActivityManager.getService();
+                for (ActivityManager.RunningAppProcessInfo app: am.getRunningAppProcesses()) {
+                    if ("com.android.systemui".equals(app.processName)) {
+                        ams.killApplicationProcess(app.processName, app.uid);
+                        break;
+                    }
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
     }
 
     public static void launchApp(Context context, boolean leftEdgeApp, boolean isVerticalSwipe) {
