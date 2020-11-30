279917944c8a3b7b2bdc26de1b21684213ac8494
Joey Huab <joey@evolution-x.org>
2020-12-01 06:50:16 +0900

    SystemUI: FOD pressed Colors [1/2]
    Signed-off-by: Aston-Martinn <advaithbhat9@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6790,6 +6790,12 @@ public final class Settings {
         */
         public static final String QS_TILE_STYLE = "qs_tile_style";
 
+        /**
+         * FOD pressed color
+         * @hide
+         */
+        public static final String FOD_COLOR = "fod_color";
+
 	/**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
--- packages/SystemUI/src/com/android/systemui/biometrics/FODCircleView.java
+++ packages/SystemUI/src/com/android/systemui/biometrics/FODCircleView.java
@@ -161,6 +161,15 @@ public class FODCircleView extends ImageView implements ConfigurationListener {
         R.drawable.fod_icon_transparent
     };
 
+    private int mDefaultPressedColor;
+    private int mPressedColor;
+    private final int[] PRESSED_COLOR = {
+        R.drawable.fod_icon_pressed,
+        R.drawable.fod_icon_pressed_cyan,
+        R.drawable.fod_icon_pressed_green,
+        R.drawable.fod_icon_pressed_yellow
+    };
+
     private IFingerprintInscreenCallback mFingerprintInscreenCallback =
             new IFingerprintInscreenCallback.Stub() {
         @Override
@@ -299,6 +308,9 @@ public class FODCircleView extends ImageView implements ConfigurationListener {
                     false, this, UserHandle.USER_ALL);
             resolver.registerContentObserver(Settings.System.getUriFor(
                     Settings.System.FOD_ICON), false, this, UserHandle.USER_ALL);
+            resolver.registerContentObserver(Settings.System.getUriFor(
+                    Settings.System.FOD_COLOR),
+                    false, this, UserHandle.USER_ALL);
         }
 
         void unobserve() {
@@ -307,9 +319,9 @@ public class FODCircleView extends ImageView implements ConfigurationListener {
 
         @Override
         public void onChange(boolean selfChange, Uri uri) {
-            if (uri.equals(Settings.System.getUriFor(
-                    Settings.System.FOD_ANIM)) || uri.equals(Settings.System.getUriFor(
-                Settings.System.FOD_ICON))) {
+            if (uri.equals(Settings.System.getUriFor(Settings.System.FOD_ANIM)) ||
+                    uri.equals(Settings.System.getUriFor(Settings.System.FOD_ICON)) ||
+                    uri.equals(Settings.System.getUriFor(Settings.System.FOD_COLOR))) {
                 updateStyle();
             // if (uri.equals(Settings.System.getUriFor(SCREEN_BRIGHTNESS))) {
             update();
@@ -394,12 +406,11 @@ public class FODCircleView extends ImageView implements ConfigurationListener {
             @Override
             protected void onDraw(Canvas canvas) {
                 if (mIsCircleShowing) {
-                    canvas.drawCircle(mSize / 2, mSize / 2, mSize / 2.0f, mPaintFingerprint);
+                    setImageResource(PRESSED_COLOR[mPressedColor]);
                 }
                 super.onDraw(canvas);
             }
         };
-        mPressedView.setImageResource(R.drawable.fod_icon_pressed);
 
         mWindowManager.addView(this, mParams);
 
@@ -665,6 +676,9 @@ public class FODCircleView extends ImageView implements ConfigurationListener {
                 Settings.System.FOD_RECOGNIZING_ANIMATION, 0) != 0;
         mSelectedIcon = Settings.System.getInt(mContext.getContentResolver(),
                 Settings.System.FOD_ICON, 0);
+        mPressedColor = Settings.System.getInt(mContext.getContentResolver(),
+                Settings.System.FOD_COLOR, mDefaultPressedColor);
+
         if (mFODAnimation != null) {
             mFODAnimation.update();
         }
