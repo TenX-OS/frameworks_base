359f6c09f00902668d0ef3b34ae4e6cec583deb4
Bruno Martins <bgcngm@gmail.com>
2020-12-26 03:45:47 +0530

    base: [LineageHardwareService] Publish binder service during onStart()
    According to the lifecycle of a SystemService, onStart() is called to get
    the service running and the service should publish its binder interface
    at that point.
    
    Change-Id: I99031807f45d8d7e048582749b1ba6885aa12af4
    Signed-off-by: Aston-Martinn <advaithbhat9@gmail.com>

--- services/core/java/com/android/server/custom/LineageHardwareService.java
+++ services/core/java/com/android/server/custom/LineageHardwareService.java
@@ -90,7 +90,6 @@ public class LineageHardwareService extends SystemService {
         super(context);
         mContext = context;
         mLineageHwImpl = getImpl(context);
-        publishBinderService(LineageContextConstants.LINEAGE_HARDWARE_SERVICE, mService);
     }
 
     @Override
@@ -105,6 +104,7 @@ public class LineageHardwareService extends SystemService {
 
     @Override
     public void onStart() {
+        publishBinderService(LineageContextConstants.LINEAGE_HARDWARE_SERVICE, mService);
     }
 
     private final IBinder mService = new ILineageHardwareService.Stub() {
