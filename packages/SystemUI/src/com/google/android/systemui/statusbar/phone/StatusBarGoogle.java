3b5a11ea4dc5d2e86d7feeb214dbee241c9abb30
Advaith Bhat <advaithbhat9@gmail.com>
2021-02-09 03:13:03 +0000

    SystemUI: Remove Smartspace
    - For OmniJaws weather impl.
    
    This reverts commit a340db53d94d8defe633829e54c88b43b467e712.
    
    Signed-off-by: Aston-Martinn <advaithbhat9@gmail.com>
    Change-Id: If84ec0a7406ca9d774654b5d57e01ad4c8281653

--- packages/SystemUI/src/com/google/android/systemui/statusbar/phone/StatusBarGoogle.java
+++ packages/SystemUI/src/com/google/android/systemui/statusbar/phone/StatusBarGoogle.java
@@ -16,7 +16,6 @@ import com.android.internal.logging.MetricsLogger;
 import com.android.keyguard.KeyguardUpdateMonitor;
 import com.android.keyguard.ViewMediatorCallback;
 import com.android.systemui.dagger.qualifiers.UiBackground;
-import com.android.systemui.Dependency;
 import com.android.systemui.InitController;
 import com.android.systemui.assist.AssistManager;
 import com.android.systemui.broadcast.BroadcastDispatcher;
@@ -93,13 +92,8 @@ import com.android.systemui.statusbar.policy.UserSwitcherController;
 import com.android.systemui.tuner.TunerService;
 import com.android.systemui.volume.VolumeComponent;
 
-import com.google.android.systemui.NotificationLockscreenUserManagerGoogle;
-import com.google.android.systemui.smartspace.SmartSpaceController;
-
 import dagger.Lazy;
 
-import java.io.FileDescriptor;
-import java.io.PrintWriter;
 import java.util.Optional;
 import java.util.concurrent.Executor;
 
@@ -109,11 +103,7 @@ import javax.inject.Inject;
 
 public class StatusBarGoogle extends StatusBar {
 
-    @Inject
-    public SmartSpaceController mSmartSpaceController;
-
     public StatusBarGoogle(
-            SmartSpaceController smartSpaceController,
             Context context,
             NotificationsController notificationsController,
             LightBarController lightBarController,
@@ -275,24 +265,15 @@ public class StatusBarGoogle extends StatusBar {
                 statusBarTouchableRegionManager,
                 tunerService,
                 taskHelper);
-        mSmartSpaceController = smartSpaceController;
     }
 
     @Override
     public void start() {
         super.start();
-        ((NotificationLockscreenUserManagerGoogle) Dependency.get(NotificationLockscreenUserManager.class)).updateSmartSpaceVisibilitySettings();
     }
 
     @Override
     public void setLockscreenUser(int i) {
         super.setLockscreenUser(i);
-        mSmartSpaceController.reloadData();
-    }
-
-    @Override
-    public void dump(FileDescriptor fileDescriptor, PrintWriter printWriter, String[] strArr) {
-        super.dump(fileDescriptor, printWriter, strArr);
-        mSmartSpaceController.dump(fileDescriptor, printWriter, strArr);
     }
 }
