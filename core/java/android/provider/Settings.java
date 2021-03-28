8100e5fbc99891eed0d22e7be108850dc9db9e88
Advaith Bhat <advaithbhat9@gmail.com>
2021-03-28 06:18:09 +0000

    base: Remove AOKP & Ethan's animations [1/2]
    Revert "base: Add Ethans animations [1/2]"
    
    This reverts commit 8b5ff43366686942818c95095e5082c8d7056a97.
    
    Revert "base: AOKP custom system animations (1/2)"
    
    This reverts commit ce49f3920ce0a8885d72d8e7fee5f5eec609a929.

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -15432,25 +15432,6 @@ public final class Settings {
         public static final String BATTERY_LIGHT_ENABLED = "battery_light_enabled";
 
         /**
-         * AOKP Custom System Animations
-         * @hide
-         */
-        public static final String[] ACTIVITY_ANIMATION_CONTROLS = new String[] {
-                "activity_open",
-                "activity_close",
-                "task_open",
-                "task_close",
-                "task_to_front",
-                "task_to_back",
-                "wallpaper_open",
-                "wallpaper_close",
-                "wallpaper_intra_open",
-                "wallpaper_intra_close",
-                "task_open_behind",
-        };
-        public static final String ANIMATION_CONTROLS_DURATION = "animation_controls_duration";
-
-        /**
          * Toast animations
          * @hide
          */
